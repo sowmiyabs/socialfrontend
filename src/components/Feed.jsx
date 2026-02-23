0e3b9c62c2b9a36e67330185cd099ace1d1f2e21
sowmiyabs <sowmiyab001@gmail.com>
2026-02-23 15:24:45 +0530

    commit

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -2,7 +2,7 @@
 import { useEffect, useState } from "react";
 import API from "../api/axios";
 import Post from "./Post";
-import PostForm from "./postForm.jsx";
+import PostForm from "./postform.jsx";
 import ErrorBoundary from "./ErrorBoundary.jsx";
 
 function FeedContent() {
