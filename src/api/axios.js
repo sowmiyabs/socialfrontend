299df42bea279242ad935e0234985f5bbc92eda3
sowmiyabs <sowmiyab001@gmail.com>
2026-03-10 12:03:11 +0530

    commit

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,14 +1,18 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: import.meta.env.VITE_API_URL,
   headers: { "Content-Type": "application/json" },
 });
 
-// Interceptor to attach token
+// Attach token automatically
 API.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token"); // or wherever you store it
-  if (token) config.headers.Authorization = `Bearer ${token}`;
+  const token = localStorage.getItem("token");
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
   return config;
 });
 
